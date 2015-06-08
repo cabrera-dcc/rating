e069252cfab35fb48459591be6dd418381c84236
cabrera-dcc <danic.cabrera@gmail.com>
2015-06-08 16:36:59 +0200

    Basic index, js, css and font files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Rating System</title>
+	<meta name="application-name" content="Rating"/>
+    <meta name="description" content="Sistema de valoración de projectos"/>
+    <meta name="author" content="Daniel Cabrera Cebrero (http://cabrera-dcc.github.io)"/>
+    <meta name="version" content="Alpha (rev. 20150608)"/>
+    <meta name="keywords" content="opensource,rating,projects"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
+</head>
+<body>
+	<header></header>
+
+	<nav></nav>
+
+	<main>
+		<section></section>
+	</main>
+
+	<footer></footer>
+
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
