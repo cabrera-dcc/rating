5b7072dce0e67f2d8099d1d42fd23f7b373e6938
cabrera-dcc <danic.cabrera@gmail.com>
2015-06-12 00:09:10 +0200

    Login style modified and tag attributes fixed up (b1r20150612)

--- includes/option_icons.php
+++ includes/option_icons.php
@@ -1,4 +1,3 @@
 <?php
-
-echo "<a class='start_option' href='indexadmin.php?option=start&ref=" . $row['id'] . "' title='Abrir para valorar'><span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span></a> | ";
-echo "<a class='close_option' href='indexadmin.php?option=close&ref=" . $row['id'] . "' title='Abrir para valorar'><span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span></a> ";
+echo "<a class='start_option' rel='bookmark' target='_self' hreflang='es' href='indexadmin.php?option=start&ref=" . $row['id'] . "' title='Abrir para valorar'><span class='glyphicon glyphicon-play' aria-hidden='true'></span></a> | ";
+echo "<a class='close_option' rel='bookmark' target='_self' hreflang='es' href='indexadmin.php?option=close&ref=" . $row['id'] . "' title='Cerrar la valoración'><span class='glyphicon glyphicon-stop' aria-hidden='true'></span></a> ";
